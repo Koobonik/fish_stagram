41f98d99153cf1dcf9b6854dfd02146d58b24ee4
Koobonik <kbi960130@naver.com>
2018-11-19 13:07:48 +0900

    데스크탑으로 작업하자

--- src/main/java/com/devkoo/webservice/web/WebController.java
+++ src/main/java/com/devkoo/webservice/web/WebController.java
@@ -18,15 +18,21 @@ public class WebController { // 웹컨트롤러로써 GetMapping 이용해서 �
 
     // 메인뷰
     //fineAllDesc 가 오름차순으로 정렬해줄 것임
-    @RequestMapping(value="/", method = {RequestMethod.GET, RequestMethod.POST})
-    public String main(Model model) {
+    @RequestMapping(value="/index", method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
-        return "main";
+        return "instagram";
     }
 
-    @GetMapping("/hello") // 웹에서 /hello 라는 요청이 있을경우
+    @RequestMapping(value="/", method = {RequestMethod.GET, RequestMethod.POST})
+    public String main(Model model) {
+        //model.addAttribute("posts", postsService.findAllDesc());
+        return "instagram";
+    }
+
+    @GetMapping("/instagram") // 웹에서 /hello 라는 요청이 있을경우
     public String hello() {
-        return "hello";
+        return "instagram";
     }
     // 회원가입 페이지
     @RequestMapping(value="signup", method = {RequestMethod.GET, RequestMethod.POST})
